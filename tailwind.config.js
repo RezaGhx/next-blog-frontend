9d8429c9c3ad96ef6bf1bbe6b8ebe0e61e3bae42
r.ghafouri <reza99gh@gmail.com>
2023-09-16 18:09:33 +0330

    tailwind config updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,39 @@ module.exports = {
       fontFamily: {
         sans: ['iranyekan'],
       },
+      // colors: {
+      //   'blue': '#1fb6ff',
+      //   'pink': '#ff49db',
+      //   'orange': '#ff7849',
+      //   'green': '#13ce66',
+      //   'gray-dark': '#273444',
+      //   'gray': '#8492a6',
+      //   'gray-light': '#d3dce6',
+      // },
+      //
+      // darkMode: 'className',
+      //
+      // screens: {
+      //   sm: '480px',
+      //   md: '768px',
+      //   lg: '976px',
+      //   xl: '1440px',
+      // },
+      //
+      // spacing: {
+      //   '128': '32rem',
+      //   '144': '36rem',
+      // },
+      //
+      // borderRadius: {
+      //   '4xl': '2rem',
+      // }
     },
   },
-  plugins: [],
-}
+  plugins: [
+    // require('@tailwindcss/forms'),
+    // require('@tailwindcss/aspect-ratio'),
+    // require('@tailwindcss/typography'),
+    // require('tailwindcss-children'),
+  ],
+};
