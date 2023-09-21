6c3810c3d42c5c5042c053b40ece7314246cd843
r.ghafouri <reza99gh@gmail.com>
2023-09-21 17:10:29 +0330

    bookmark, like, and other post accessories have been added

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,19 @@ import styles from 'styles/Home.module.css';
 import {
   ChevronDownIcon,
   AdjustmentsHorizontalIcon,
+  // BookmarkIcon,
+  ChatBubbleBottomCenterTextIcon,
+  // HeartIcon,
+  ClockIcon,
 } from '@heroicons/react/24/outline';
+import {
+  // ChevronDownIcon,
+  // AdjustmentsHorizontalIcon,
+  BookmarkIcon,
+  // ChatBubbleBottomCenterTextIcon,
+  HeartIcon,
+  // ClockIcon,
+} from '@heroicons/react/24/solid';
 import Link from 'next/link';
 import Image from 'next/image';
 import { useState } from 'react';
@@ -103,7 +115,7 @@ export default function Home() {
                   <div className={'bg-gray-50 rounded-2xl p-2'}>
                     <h2 className={'mb-4'}>بررسی کامل ریکت و ریکت نیتیو</h2>
                   </div>
-                  <div className={'flex items-center justify-between'}>
+                  <div className={'flex items-center justify-between mb-7'}>
                     <div className={'flex items-center'}>
                       <img
                         src={'/images/nextjs.png'}
@@ -112,7 +124,9 @@ export default function Home() {
                           'rounded-full w-6 h-6 ring-2 ring-slate-500 ml-2'
                         }
                       />
-                      <span className={'text-sm text-slate-500'}>رضا غفوری</span>
+                      <span className={'text-sm text-slate-400'}>
+                        رضا غفوری
+                      </span>
                     </div>
                     <span
                       className={
@@ -121,6 +135,37 @@ export default function Home() {
                     >
                       جاوااسکریپت
                     </span>
+                  </div>
+                  <div className={'flex items-center justify-between'}>
+                    <div className={'flex text-xs'}>
+                      <span
+                        className={
+                          'flex px-2 py-1 rounded-xl bg-red-100 text-red-600 hover:bg-red-600 hover:text-red-100 transition-all duration-300 cursor-pointer'
+                        }
+                      >
+                        12
+                        <HeartIcon className={'w-4 h-4 mr-2'} />
+                      </span>
+                      <span
+                        className={
+                          'flex px-2 py-1 rounded-xl bg-slate-100 text-slate-600 hover:bg-slate-600 hover:text-slate-100 transition-all duration-300 cursor-pointer'
+                        }
+                      >
+                        9
+                        <ChatBubbleBottomCenterTextIcon className={'w-4 h-4 mr-2'} />
+                      </span>
+                      <span
+                        className={
+                          'flex px-2 py-1 rounded-xl bg-blue-100 text-blue-600 hover:bg-blue-600 hover:text-blue-100 transition-all duration-300 cursor-pointer'
+                        }
+                      >
+                        <BookmarkIcon className={'w-4 h-4'} />
+                      </span>
+                    </div>
+                    <div className={'flex items-center text-slate-400'}>
+                      <ClockIcon className={'w-4 h-4 ml-1'} />
+                      <span className={'text-xs'}>زمان مطالعه 20 دقیقه</span>
+                    </div>
                   </div>
                 </div>
               );
